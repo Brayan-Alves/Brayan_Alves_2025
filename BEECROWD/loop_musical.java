9ac094fa5bfccf02defd24db632719e7b0504b8c
Brayan_Alves <brayanalvesdossantos67@gmail.com>
2025-04-25 15:29:48 -0300

    feito

--- BEECROWD/loop_musical.java
+++ BEECROWD/loop_musical.java
@@ -6,7 +6,7 @@ public class loop_musical {
     final static Scanner LER = new Scanner(System.in);
     public static void main(String[] args) {
         
-        while (true) { //LOOOOOOOOOOOOOOOOOOOOop
+        while (true) { 
             
             int n = LER.nextInt();
             int picos = 0;
@@ -22,34 +22,15 @@ public class loop_musical {
 
             int[] altitudes = new int[n];
 
-            for (int i = 0; i < altitudes.length; i++) {
-                altitudes[i] = LER.nextInt();
-            }
-
-            if(altitudes[0] > altitudes[1]){
-                inicioLoop = true;
-            }
-            if(altitudes[altitudes.length - 2] > altitudes[altitudes.length - 1]){
-                fimLoop = true;
-            }
+            altitudes = lerVetInt(altitudes);
+            inicioLoop = verificarInicioDoLoop(altitudes);
+            fimLoop = verificarFimDoLoop(altitudes);
 
             altitudePasssadaA = altitudes[0];
             altitudePasssadaB = altitudes[1];
             picos++;
 
-            for (int i = 2; i < altitudes.length; i++) {
-                if(altitudePasssadaA > altitudePasssadaB){
-                    if(altitudes[i] > altitudePasssadaB){
-                        picos++;
-                    }
-                }else{
-                    if(altitudes[i] < altitudePasssadaB){
-                        picos++;
-                    }
-                }
-                altitudePasssadaA = altitudePasssadaB;
-                altitudePasssadaB = altitudes[i];
-            }
+            picos = contarPicos(altitudes, altitudePasssadaA, altitudePasssadaB, picos);
 
             if(inicioLoop && fimLoop || !inicioLoop && !fimLoop){
                 picos++;
@@ -59,5 +40,54 @@ public class loop_musical {
                 
         } 
     }   
+
+    public static int[] lerVetInt(int[] vet){
+        for (int i = 0; i < vet.length; i++) {
+            vet[i] = lerInt();
+        }
+        return vet;
+    }
+
+    public static int lerInt(){
+        return LER.nextInt();
+    }
+
+    public static boolean verificarInicioDoLoop(int[] altitudes){
+        boolean inicioLoop = false;
+
+        if(altitudes[0] > altitudes[1]){
+            inicioLoop = true;
+        }
+
+        return inicioLoop;
+    }
+
+    public static boolean verificarFimDoLoop(int[] altitudes){
+        boolean fimLoop = false;
+
+        if(altitudes[altitudes.length - 2] > altitudes[altitudes.length - 1]){
+            fimLoop = true;
+        }
+
+        return fimLoop;
+    }
+
+    public static int contarPicos(int[] altitudes, int altitudePasssadaA, int altitudePasssadaB, int picos){
+        for (int i = 2; i < altitudes.length; i++) {
+            if(altitudePasssadaA > altitudePasssadaB){
+                if(altitudes[i] > altitudePasssadaB){
+                    picos++;
+                }
+            }else{
+                if(altitudes[i] < altitudePasssadaB){
+                    picos++;
+                }
+            }
+            altitudePasssadaA = altitudePasssadaB;
+            altitudePasssadaB = altitudes[i];
+        }
+
+        return picos;
+    }
 }
 
