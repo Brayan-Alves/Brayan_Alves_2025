40794510c0b8170e50f5694eb96015bcd1fe37be
Brayan_Alves <brayanalvesdossantos67@gmail.com>
2025-06-09 09:55:16 -0300

    s

--- OBI_2024/frutas.java
+++ OBI_2024/frutas.java
@@ -6,6 +6,16 @@ public class frutas {
     public static Scanner LER = new Scanner(System.in);
     public static void main(String[] args) {
         int dinheiro = LER.nextInt();
-        
+        int frutas = LER.nextInt();
+        int[][] tabela = new int[frutas][2];
+        for (int i = 0; i < frutas; i++) {
+            tabela[i][0] = LER.nextInt();
+            tabela[i][1] = LER.nextInt();
+        }
+        for (int i = 0; i < tabela.length; i++) {
+            for (int j = 0; j < tabela.length; j++) {
+                
+            }
+        }
     }
 }
