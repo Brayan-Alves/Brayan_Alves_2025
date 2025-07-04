f96aad579c646295f0a48715f8590bc9f4458b9e
Brayan_Alves <brayanalvesdossantos67@gmail.com>
2025-07-04 15:16:52 -0300

    sim

---
+++ BEECROWD/lista_de_compras.java
@@ -0,0 +1,17 @@
+package BEECROWD;
+import java.util.*;
+
+public class lista_de_compras {
+    public static Scanner LER = new Scanner(System.in);
+    public static void main(String[] args) {
+        
+        
+        int n = LER.nextInt();
+        for (int i = 0; i < n; i++) {
+            TreeSet<String> lista = new TreeSet<>();
+            String linha = LER.nextLine();
+            lista = linha.
+        }
+    }
+    
+}
