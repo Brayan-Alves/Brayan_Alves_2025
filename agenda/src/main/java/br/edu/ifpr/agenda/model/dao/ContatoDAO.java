28c03a4503937309b60e9aec56b724a38335fb51
Brayan_Alves <brayanalvesdossantos67@gmail.com>
2025-11-03 09:45:07 -0300

    sim

--- agenda/src/main/java/br/edu/ifpr/agenda/model/dao/ContatoDAO.java
+++ agenda/src/main/java/br/edu/ifpr/agenda/model/dao/ContatoDAO.java
@@ -1,12 +1,15 @@
 package br.edu.ifpr.agenda.model.dao;
 
-import br.edu.ifpr.agenda.model.Contato;
-
 import java.sql.Statement;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.sql.ResultSet;
+
+import java.sql.SQLException;
+
+import br.edu.ifpr.agenda.model.Contato;
+import br.edu.ifpr.agenda.model.Endereco;
 
 public class ContatoDAO {
     //Create
@@ -29,10 +32,10 @@ public class ContatoDAO {
 
     public void salvar(Contato contato){
         Connection con = ConnectionFactory.getConnection();
-        String sqlEndereco = "INSERT INTO endereco(rua, numero, cidade, estados) VALUES(?,?,?,?)";
+        String sqlEndereco = "INSERT INTO enderecos(rua, numero, cidade, estado) VALUES(?,?,?,?)";
         try {
-            PreparedStatement psEndereco = 
-                    con.prepareStatement(sqlEndereco,Statement.RETURN_GENERATED_KEYS);
+            PreparedStatement psEndereco = con.prepareStatement(sqlEndereco,Statement.RETURN_GENERATED_KEYS);
+            Endereco end = contato.getEndereco();
             psEndereco.setString(1, contato.getEndereco().getRua());
             psEndereco.setString(2, contato.getEndereco().getNumero());
             psEndereco.setString(3, contato.getEndereco().getCidade());
@@ -49,6 +52,8 @@ public class ContatoDAO {
             psContato.setString(2, contato.getCelular());
             psContato.setString(3, contato.getCelular());
             psContato.setInt(4, idEndereco);
+            psContato.executeUpdate();
+            System.out.println("Tudo certo parça");
         } catch (SQLException e) {
             // TODO: handle exception
             e.printStackTrace();
